f7ddc10e9100d6de2274d1bfa4a56c734dbaf400
Jimmy <1308075897@qq.com>
2025-05-06 19:50:17 +0800

    ok

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="mt-12 pt-6 border-t border-zinc-100 dark:border-zinc-800">
+    <footer className="mt-12 pt-6">
       <div className="text-center text-zinc-400 dark:text-zinc-500 text-xs py-4">
         <div className="flex justify-center gap-4 items-center">
           <p>© {new Date().getFullYear()} Designed By Jimmy</p>
