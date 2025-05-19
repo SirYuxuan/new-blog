2d2434b6ff412781332a932b25fee6bc0aa1809d
Jimmy <1308075897@qq.com>
2025-05-19 12:21:20 +0800

    refactor(tailwind.config.ts, globals.css):update post title in ashes.md;

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -146,8 +146,7 @@ const config: Config = {
               marginBottom: "0.75rem",
             },
             img: {
-              borderRadius: "0.375rem",
-              boxShadow: "0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)",
+              // 移除所有样式，保持图片原始状态
             },
             ul: {
               listStyleType: "disc",
